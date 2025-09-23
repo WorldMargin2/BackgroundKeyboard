db31e7e1107680531684a8eda0c6e98d2579960a
WorldMargin2 <148187588+WorldMargin2@users.noreply.github.com>
2025-09-23 23:23:23 +0800

    添加项目文件。

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KeysBinding;
+
+namespace BackgroundKeyboard {
+    public partial class Form1 : Form {
+
+        private KeyGridBinding binding;
+        public Form1() {
+            InitializeComponent();
+            binding = new KeyGridBinding(dataGridView1);
+            binding.startListen();
+            this.FormClosed += (s, e) => {
+                binding.stopListen();
+            };
+            bind();
+            binding.initGridData();
+        }
+
+        void bind() {
+            binding.bind("666", () => {
+                MessageBox.Show("666");
+            },Keys.Alt,Keys.NumPad6);
+
+            binding.bind("777", () => {
+                MessageBox.Show("777");
+            }, Keys.Alt, Keys.NumPad7);
+        }
+    }
+}
